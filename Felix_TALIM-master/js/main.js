28c09ffaca81301b5a07c71c6780e8a1879adf02
Ibrohimjon <115447690+Ibrohimqor1@users.noreply.github.com>
2022-11-19 13:33:40 +0500

    Add files via upload

---
+++ Felix_TALIM-master/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 0,
+        nav: false,
+      },
+      500: {
+        items: 1,
+        nav: false,
+      },
+      1000: {
+        items: 2,
+        nav: false,
+      },
+    },
+  });
+});
